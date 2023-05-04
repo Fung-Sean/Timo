70ba3df3a0376821142c6f35422e3819273a9642
rithvik213 <33884560+rithvik213@users.noreply.github.com>
2023-05-04 19:14:19 -0400

    weather fixed and done (#42)
    Co-authored-by: JulianTanja <61524227+JulianTanja@users.noreply.github.com>

--- lib/app/modules/homescreen/controllers/homescreen_controller.dart
+++ lib/app/modules/homescreen/controllers/homescreen_controller.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:googleapis/calendar/v3.dart';
@@ -15,17 +16,18 @@ import 'package:timo_test/app/modules/login/controllers/login_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:timo_test/notification_serviceController.dart';
+//import 'package:timo_test/notification_serviceController.dart';
 import 'dart:math';
 import '../../onboarding/controllers/onboarding_controller.dart';
 import 'dart:math';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+//import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 //import '../../../../notification_service.dart';
 //import '../../../../notification_service_cont.dart';
-import '../../../../notification_serviceController.dart';
+//import '../../../../notification_serviceController.dart';
 import 'package:async/async.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:http/http.dart' as http;
@@ -448,6 +450,47 @@ class HomescreenController extends GetxController {
     super.onClose();
   }
 
+  //Notifications
+  void sendStartNotification() {
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
+
+    AwesomeNotifications().createNotification(
+        content: NotificationContent(
+            id: 1,
+            channelKey: 'test_channel',
+            title: 'TIMO',
+            body: 'Timer has started!'));
+
+    //what happens if we click the notif?
+    // AwesomeNotifications().actionStream.listen((event) {
+    //   Get.to(const Intro1View());
+    // });
+  }
+
+  void sendFinishNotification() {
+    AwesomeNotifications().isNotificationAllowed().then((isAllowed) {
+      if (!isAllowed) {
+        AwesomeNotifications().requestPermissionToSendNotifications();
+      }
+    });
+
+    AwesomeNotifications().createNotification(
+        content: NotificationContent(
+            id: 1,
+            channelKey: 'test_channel',
+            title: 'TIMO',
+            body: 'Timer has finished!'));
+
+    //what happens if we click the notif?
+    // AwesomeNotifications().actionStream.listen((event) {
+    //   Get.to(const Intro1View());
+    // });
+  }
+
   int convertYearToInt(String date) {
     DateTime dateTime = DateFormat('EEEE, MMMM d, y').parse(date);
     int year = dateTime.year;
@@ -478,11 +521,51 @@ class HomescreenController extends GetxController {
     return minute;
   }
 
+  //FOR NOTIFICATION/POPUPS
+  final stylebegin_title = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    color: Color.fromARGB(255, 53, 147, 255),
+  );
+
+  final stylebegin_middle = TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w300,
+      color: Color.fromARGB(255, 0, 0, 0));
+
   //function to handle timer logic for before an event's getReady timer starts
   void startBeforeGetReadyTimer() async {
     await _beforeGetReady.runOnce(() async {
       // code for your async operation here
       print("I AM IN TIMER FUNCTION");
+
+      Get.defaultDialog(
+          title: 'Your timer has begun!',
+          titleStyle: GoogleFonts.inter(textStyle: stylebegin_title),
+          content: SizedBox(
+              width: 300,
+              height: 150,
+              child: Text(
+                'Timo takes your Google Calendar \n events to set aside periods of time for \n you to get ready and travel to your \n destination on time.',
+                style: GoogleFonts.inter(textStyle: stylebegin_middle),
+                textAlign: TextAlign.center,
+              )),
+          // textConfirm: "Let's go!",
+          // buttonColor: Color.fromARGB(255, 53, 147, 255)
+          actions: [
+            ElevatedButton(
+                onPressed: () {
+                  Get.back();
+                },
+                child: Text("Let's go!"))
+          ],
+          barrierDismissible: true
+
+          //middleText:
+          //    'Timo takes your Google Calendar \n events to set aside periods of time for \n you to get ready and travel to your \n destination on time.',
+          //middleTextStyle: GoogleFonts.inter(textStyle: stylebegin_middle)
+          );
+
       SharedPreferences prefs = await SharedPreferences.getInstance();
 
       //change display show timer is over
@@ -583,6 +666,43 @@ class HomescreenController extends GetxController {
       sub.onDone(() {
         print("Done");
         sub.cancel();
+
+        Get.defaultDialog(
+            title: 'Your timers have ended!',
+            titleStyle: GoogleFonts.inter(textStyle: stylebegin_title),
+            content: SizedBox(
+                width: 300,
+                height: 90,
+                child: Text(
+                  'Did you make it to your event on time?',
+                  style: GoogleFonts.inter(textStyle: stylebegin_middle),
+                  textAlign: TextAlign.center,
+                )),
+            // textConfirm: "Let's go!",
+            // buttonColor: Color.fromARGB(255, 53, 147, 255)
+            actions: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      child: Text("Nope")),
+                  ElevatedButton(
+                      onPressed: () {
+                        Get.back();
+                      },
+                      child: Text("Yes!"))
+                ],
+              )
+            ],
+            barrierDismissible: true
+
+            //middleText:
+            //    'Timo takes your Google Calendar \n events to set aside periods of time for \n you to get ready and travel to your \n destination on time.',
+            //middleTextStyle: GoogleFonts.inter(textStyle: stylebegin_middle)
+            );
 
         //make reset method but for now
         aboveTimer.value = "Get Ready!";
--- lib/app/modules/weatherpage/views/weatherpage_view.dart
+++ lib/app/modules/weatherpage/views/weatherpage_view.dart
@@ -23,41 +23,61 @@ class WeatherpageView extends GetView<WeatherpageController> {
       fontWeight: FontWeight.w500,
       color: Color.fromARGB(255, 53, 147, 255));
 
-  final _weatherpagecontroller = Get.find<WeatherpageController>();
+  //final _weatherpagecontroller = Get.find<WeatherpageController>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Main',
-              style: TextStyle(
-                color: Color.fromARGB(0, 0, 0, 0),
-              )),
-          centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        title: const Text(
+          'Main',
+          style: TextStyle(
+            color: Color.fromARGB(0, 0, 0, 0),
+          ),
         ),
-        body: Obx(() => _weatherpagecontroller.isLoading.value
-            ? Center(
-                child: Column(
-                children: <Widget>[
-                  const SizedBox(height: 80),
-                  Text(dt,
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.inter(textStyle: style_date)),
-                  const SizedBox(height: 100),
-
-                  Text('${_weatherpagecontroller.futureWeather[0].temp}°C',
-                      style: GoogleFonts.inter(textStyle: style_temp)),
-                  const SizedBox(height: 10),
-                  //Text(
-                  //    'H: ${_weathercontroller.futureWeather[0].temp_max}°C  L: ${_weathercontroller.futureWeather[0].temp_min}°C',
-                  //    style: GoogleFonts.inter(textStyle: style_temp_hl)),
-                  // ignore: unnecessary_string_interpolations
-                  //Text(
-                  //    'Description: ${_weathercontroller.futureWeather[0].w_description}',
-                  //    style: GoogleFonts.inter(textStyle: style_temp_hl))
-                ],
-              ))
-            : const CircularProgressIndicator()));
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      ),
+      body: FutureBuilder(
+        future: controller.getWeather(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            print("Waiting for weather data!");
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasError) {
+            print("I am in snapshot error");
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+          return Obx(
+            () => Column(
+              children: <Widget>[
+                const SizedBox(height: 80),
+                Text(
+                  dt,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.inter(textStyle: style_date),
+                ),
+                const SizedBox(height: 100),
+                /*
+                Text(
+                  '${controller.futureWeather[0].temp_f}°F',
+                  style: GoogleFonts.inter(textStyle: style_temp),
+                ),
+                */
+                const SizedBox(height: 10),
+                //Text(
+                //    'H: ${_weathercontroller.futureWeather[0].temp_max}°C  L: ${_weathercontroller.futureWeather[0].temp_min}°C',
+                //    style: GoogleFonts.inter(textStyle: style_temp_hl)),
+                // ignore: unnecessary_string_interpolations
+                //Text(
+                //    'Description: ${_weathercontroller.futureWeather[0].w_description}',
+                //    style: GoogleFonts.inter(textStyle: style_temp_hl))
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
